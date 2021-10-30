c4758e17e42936251c83185081eee63f10ad143f
Noce <enrico.mannocci99@gmail.com>
2021-10-30 12:16:40 +0200

    Aggiunta/preparata definizione del min_max!

--- min_max.py
+++ min_max.py
@@ -1,9 +1,19 @@
+import player
+
+
 # state -> matrice 9x9 con lo stato della partita
-# color -> "W" or "B"
+# player -> instance of Player (self.B or self.W where self is Game)
 
 # return move
 #   black move -> random_soldier, None, random_move
 #   king move  -> return "K", None, random_move
 #   white move -> return "S", random_soldier, random_move
-def min_max_player(state, color):
-    print("Sono il minmax e ti ucciderò!")
+def min_max_player(state, pl: player.Player):
+    print("Sono il min_max e ti ucciderò!")
+    find_all_possible_moves(state, pl)
+
+
+def find_all_possible_moves(state, pl: player.WPlayer):
+    next_status = []
+    for i, e in enumerate(pl.soldiers):
+        moves = pl.moves[i]
