58dc5de88ef47ac13113cde2e40c02f9692eff94
Paulaner99 <rikipaolini99@gmail.com>
2021-11-27 16:21:29 +0100

    Close game early

--- src/headers/min_max.hpp
+++ src/headers/min_max.hpp
@@ -8,7 +8,7 @@
 std::string min_max_player_wrapper(std::vector<std::vector<char>>, bool, int, time_t);
 void print_state(std::vector<std::vector<char>>);
 bool get_if_state_is_a_finish_game_state(std::vector<std::vector<char>>);
-int state_evaluation(std::vector<std::vector<char>>);
+int state_evaluation(std::vector<std::vector<char>>, int);
 std::vector<int> get_king_position_on_board(std::vector<std::vector<char>>);
 std::tuple<std::tuple<std::vector<std::vector<char>> , std::vector<int>, std::vector<int>>, int> min_max(std::tuple<std::vector<std::vector<char>>, std::vector<int>, std::vector<int>>, int, int, int, int, bool);
 std::string get_move_from_matrix(std::tuple<std::vector<std::vector<char>> , std::vector<int>, std::vector<int>>);
